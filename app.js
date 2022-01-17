3f708cf83b71ba97aa98b42c50c8ee9dca55d980
yuji1016-momo <kilroy.was.here1016gmail.com>
2022-01-17 21:32:10 +0900

    ログイン機能の実装

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-const PORT = process.env.PORT;
+const IS_PRODUCTION = process.env.NODE_ENV === "production";
+const appconfig = require("./config/application.config.js");
+const dbconfig = require("./config/mysql.config.js");
 const path = require("path");
 const logger = require("./lib/log/logger.js");
 const accesslogger = require("./lib/log/accesslogger.js");
 const applicationlogger = require("./lib/log/applicationlogger.js");
+const accesscontrol = require("./lib/security/accesscontrol.js");
 const express = require("express");
+const { MySQLClient } = require("./lib/database/client.js");
+const cookie = require("cookie-parser");
+const session = require("express-session");
+const MySQLStore = require("express-mysql-session")(session);
+const flash = require("connect-flash");
 const app = express();
 
-
+// Expressの設定
 app.set('view engine','ejs');
 app.disable("x-powered-by");
 
@@ -17,8 +25,32 @@ app.use("/public",express.static(path.join(__dirname,"public")));
 // アクセスログ
 app.use(accesslogger());
 
+// ミドルウェアの設定
+app.use(cookie());
+app.use(session({
+    store: new MySQLStore({
+        host:dbconfig.HOST,
+        port:dbconfig.PORT,
+        user:dbconfig.USERNAME,
+        password:dbconfig.PASSWORD,
+        database:dbconfig.DATABASE
+    }),
+    cookie:{
+        secure:IS_PRODUCTION
+    },
+    secret: appconfig.security.SESSION_SECRET,
+    resave:false,
+    saveUninitialized: true,
+    name: "sid"
+}))
+app.use(express.urlencoded({ extended: true })); //Postを読み解くために必要
+app.use(flash());
+app.use(...accesscontrol.initialize());
+
+
 // 動的コンテンツの配信
 app.use('/',require("./routes/index.js"));
+app.use("/account",require("./routes/account.js"));
 app.use("/test",async(req,res,next)=>{
     const sqlQuery = require("./lib/database/sql/TEST_SELECT.js").sqlQuery;
     const MySQLClient = require("./lib/database/client.js").MySQLClient;
@@ -37,6 +69,6 @@ app.use("/test",async(req,res,next)=>{
 app.use(applicationlogger());
 
 // サーバー起動
-app.listen(PORT,()=>{
-    logger.application.info(`Application listening at ${PORT}`);
+app.listen(appconfig.PORT,()=>{
+    logger.application.info(`Application listening at ${appconfig.PORT}`);
 });
---
+++ routes/account.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {authenticate,authorize,PRIVILEGE} = require("../lib/security/accesscontrol.js");
+
+router.get("/",authorize(PRIVILEGE.NORMAL),(req,res,next)=>{
+    res.render("./account/index.ejs");
+})
+
+router.get("/login",(req,res,next)=>{
+    res.render("./account/login.ejs",{"message": req.flash("message")});
+});
+
+router.post("/logout",(req,res,next)=>{
+    req.logout();
+    res.redirect("/account/login");
+})
+
+router.post("/login",authenticate());
+
+module.exports = router;
